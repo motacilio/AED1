3d2fc869bfe79c5418432d10df0ad74c7aefdc7e
soproxy <marcosotacilio1@gmail.com>
2023-09-29 17:01:25 -0300

    exercicio 2 feito

---
+++ ex02 - maiorNum.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int num1, num2, maior, aux1, aux2, maiorInv = 0;
+    
+ 
+    while(1){
+        printf("Digite os numeros: ");
+        scanf("%d %d", &num1, &num2);
+        if((num1 <= 999 && 100 <= num1) && (num2 <= 999 && 100 <= num2)){
+            aux1 = num1;
+            aux2 = num2;
+            break;
+        } else{
+            printf("Digite apenas numeros de 3 digitos: \n");
+        }
+    }
+
+    while(1){
+        if(aux1 % 10 > aux2 % 10){
+            maior = num1;
+            break;
+        } else if(aux1 % 10 < aux2 % 10){
+            maior = num2;
+            break;
+        } else{
+            aux1 /= 10;
+            aux2 /= 10;
+        }
+    }
+    
+
+    for(int i = 100; maior != 0; i /= 10){
+        maiorInv += (maior % 10) * i;
+        maior /= 10;
+    }
+  
+    printf("%d", maiorInv);
+
+}
