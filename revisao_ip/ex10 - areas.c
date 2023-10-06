dc67ed6e08959fb29e1769451c1849800de2d5da
soproxy <marcosotacilio1@gmail.com>
2023-10-06 19:27:36 -0300

    ex10 - areas. Finalizado

---
+++ revisao_ip/ex10 - areas.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+
+#define PI 3.14159265
+
+int main(){
+
+    int n;
+    scanf("%d", &n);
+
+    int *resultados = (int *) calloc(n,  sizeof(int));
+
+    for(int i = 0; i < n; i++){
+
+        char fig; scanf(" %c", &fig);
+        fig = toupper(fig);
+        printf("Figura lida: %c\n", fig);
+
+        if(fig == 'C'){
+            int raio; scanf("%d", &raio);
+            double area = PI * raio * raio;
+            resultados[i] = round(area);
+            
+        } else if(fig == 'E'){
+            int raioMaior; scanf("%d", &raioMaior);
+            int raioMenor; scanf("%d", &raioMenor);
+            double area = PI * raioMaior * raioMenor;
+            resultados[i] = round(area);
+            
+        } else if(fig == 'Z'){
+            int baseMaior, baseMenor, altura;
+            scanf("%d %d %d", &baseMaior, &baseMenor, &altura);
+            double area = ((baseMaior + baseMenor) * altura) / 2;
+            resultados[i] = round(area);
+        } else if(fig == 'T'){
+            int a, b, c; scanf("%d %d %d", &a, &b, &c);
+            double semiP = (a + b + c) / 2.0;
+            double area = sqrt(semiP*(semiP - a)*(semiP - b)*(semiP - c));
+            resultados[i] = round(area);
+        }
+    }
+    
+    for(int j = 0; j < n; j++){
+    printf("%d\n", resultados[j]);
+    }
+    free(resultados);
+}
